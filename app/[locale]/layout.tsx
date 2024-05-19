f3fc4efc8b390de09d5a2e534dd232fb0ed55ae0
Aleklz89 <108018670+Aleklz89@users.noreply.github.com>
2024-05-19 15:39:26 +0300

    fixes

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -18,7 +18,12 @@ export default async function RootLayout({
   const messages = await getMessages();
   return (
     <html lang={locale}>
-      <body style={{ margin: '0' }}>
+      
+      <body style={{ 
+  margin: '0', 
+  backgroundColor: 'var(--background-color)', // Использование CSS переменной для фонового цвета
+  color: 'var(--text-color)' // Добавление цвета текста, если требуется
+}}>
       <ThemeProvider>
         <NextIntlClientProvider locale={locale} messages={messages}>
           {children}
