fd6f54b9a3429447436b6be6930c37d09f8d5a2c
Aleklz89 <108018670+Aleklz89@users.noreply.github.com>
2024-06-18 17:10:26 +0300

    fix

--- app/[locale]/(cabinet)/cabinet/transactions/page.js
+++ app/[locale]/(cabinet)/cabinet/transactions/page.js
@@ -26,7 +26,7 @@ const Transactions = () => {
           throw new Error(`Error: ${response.status}`);
         }
         const data = await response.json();
-        setUserId(data.userId);
+        setUserId(39);
       } catch (error) {
         console.error('Error fetching user ID:', error);
       }
