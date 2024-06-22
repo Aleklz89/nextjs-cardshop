2a9eda0849c66d1c064f4ecb37013b0d796713cb
Aleklz89 <108018670+Aleklz89@users.noreply.github.com>
2024-06-22 14:30:54 +0300

    fix

--- app/api/status/route.ts
+++ app/api/status/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
 import prisma from '../../lib/prisma';
 
+export const revalidate = 1
+
 export async function GET(request: NextRequest) {
   try {
     const maintenanceModeRecord = await prisma.maintenanceMode.findUnique({ where: { id: 1 } });
